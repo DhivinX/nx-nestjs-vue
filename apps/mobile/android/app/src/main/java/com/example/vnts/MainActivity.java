d53a1db048a7005dc337f4876f0453aadd6faeee
Dominik Szaradowski <d.szaradowski@gmail.com>
2022-11-15 22:17:04 +0100

    chore(mobile): add capacitor and android platform

---
+++ apps/mobile/android/app/src/main/java/com/example/vnts/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.vnts;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.example.vnts',
+    appName: 'nx-vnts',
+    webDir: '../../dist/apps/mobile',
+    bundledWebRuntime: false,
+};
+
+export default config;
