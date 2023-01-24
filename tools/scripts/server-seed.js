05c978c10acd9dc80b33c0c5cae99e0e77503555
Dominik Szaradowski <d.szaradowski@gmail.com>
2023-01-24 14:07:04 +0100

    styles: format code in seed

--- tools/scripts/server-seed.js
+++ tools/scripts/server-seed.js
@@ -16,5 +16,7 @@ shell.exec(`npx nx run server:build-cli`);
 
 for (const user of users) {
     console.log(`Create user: ${user.email}`);
-    shell.exec(`node dist/apps/server-cli/main.js newuser ${user.email} ${user.password} ${user.role} ${user.firstName} ${user.lastName}`);
+    shell.exec(
+        `node dist/apps/server-cli/main.js newuser ${user.email} ${user.password} ${user.role} ${user.firstName} ${user.lastName}`
+    );
 }
