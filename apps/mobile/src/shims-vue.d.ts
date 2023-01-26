19e2a194963f7b57204f3a4ee1a9df320c0e2f1a
Dominik Szaradowski <d.szaradowski@gmail.com>
2023-01-26 03:57:20 +0100

    chore: fix locale message import

--- apps/mobile/src/shims-vue.d.ts
+++ apps/mobile/src/shims-vue.d.ts
@@ -8,8 +8,7 @@ declare module '*.vue' {
 }
 
 declare module '@intlify/unplugin-vue-i18n/messages' {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    import { LocaleMessages } from '@intlify/core-base';
+    import { LocaleMessage } from '@intlify/core-base';
     import { VueMessageType } from 'vue-i18n';
     const messages: { [x: string]: LocaleMessage<VueMessageType> };
     export default messages;
