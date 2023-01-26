7e73e1b282c2ea543163b01aaa2eccf85f2462e9
Dominik Szaradowski <d.szaradowski@gmail.com>
2023-01-26 04:01:42 +0100

    chore(web): fix broken i18n plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import checker from 'vite-plugin-checker';
 import electron from 'vite-plugin-electron';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
@@ -51,7 +51,7 @@ export default defineConfig({
                   vueTsc: true,
               }),
 
-        vueI18n({
+        VueI18nPlugin({
             defaultSFCLang: 'yaml',
             include: resolve('./src/locales/**'),
         }),
