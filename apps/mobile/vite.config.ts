e46f0bdccaf4d9bbd2a8e951eb1a3ba4342b306e
Dominik Szaradowski <d.szaradowski@gmail.com>
2023-01-26 03:36:47 +0100

    chore: change cache dirs for vitest

--- apps/mobile/vite.config.ts
+++ apps/mobile/vite.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
     test: {
         globals: true,
         cache: {
-            dir: '../../node_modules/.vitest-mobile',
+            dir: '../../node_modules/.vitest',
         },
         environment: 'jsdom',
         include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
